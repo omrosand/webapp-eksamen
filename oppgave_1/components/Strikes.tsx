cc9287dc77455a19eb85bb73f2113a0339561aef
nattis <77730450+nattis@users.noreply.github.com>
2022-11-01 14:51:22 +0100

    added .map in components

--- oppgave_1/components/Strikes.tsx
+++ oppgave_1/components/Strikes.tsx
@@ -1,12 +1,13 @@
 // TODO: Her er det bugs
 // La til key={index} på li elementet
+// Byttet ut foreach med map
 
 export type Strike = { icon: string; guess: string }
 
 export default function Strikes({ strikes }: { strikes: Strike[] }) {
   return (
     <ul className="strikes">
-      {strikes.forEach((strike: Strike, index: number) => (
+      {strikes.map((strike: Strike, index: number) => (
         <li key={index}>{strike.icon}</li>
       ))}
     </ul>
