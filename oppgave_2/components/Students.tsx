1ffd04eed12a7b83539a50b2a78c1b3729eef4dd
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2022-11-02 15:48:44 +0100

    Laget Students komponent og lagt til litt i api endepunktet for students

---
+++ oppgave_2/components/Students.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+
+export default function Students() {
+  const [option, setOption] = useState()
+
+  const handleOptionChange = (e: any) => {
+    console.log(e.target.value)
+    setOption(e.target.value)
+  }
+
+  return (
+    <>
+      <form>
+        <section>
+          <label htmlFor="ingen">Ingen</label>
+          <input
+            type="radio"
+            id="ingen"
+            name="kategori"
+            value="ingen"
+            onClick={handleOptionChange}
+          />
+          <label htmlFor="alder">Alder</label>
+          <input
+            type="radio"
+            id="alder"
+            name="kategori"
+            value="alder"
+            onClick={handleOptionChange}
+          />
+          <label htmlFor="kjonn">Kjønn</label>
+          <input
+            type="radio"
+            id="kjonn"
+            name="kategori"
+            value="kjonn"
+            onClick={handleOptionChange}
+          />
+          <label htmlFor="klasse">Klasse</label>
+          <input
+            type="radio"
+            id="klasse"
+            name="kategori"
+            value="klasse"
+            onClick={handleOptionChange}
+          />
+        </section>
+      </form>
+      <h2>{option}</h2>
+    </>
+  )
+}
--- oppgave_2/pages/api/students.ts
+++ oppgave_2/pages/api/students.ts
@@ -1,8 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+type Student = {
+  id: string
+  name: string
+  gender: string
+  age: number
+  group: string
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  return res.status(200).json({ success: true, data: [] })
+  switch (req.method?.toLowerCase()) {
+    case 'get':
+      return res.status(200).json({ success: true, data: [] })
+    default:
+      return res
+        .status(400)
+        .json({ success: false, error: 'Method not allowed' })
+  }
 }
