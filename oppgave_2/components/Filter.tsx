1bb525417f1a0779b760fcc102a691612f19ebde
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2022-11-09 13:17:58 +0100

    Cleanup + no text for 'ingen'

---
+++ oppgave_2/components/Filter.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+export default function Filter({ setOption }: any) {
+  const handleOptionChange = (e: any) => {
+    console.log(e.target.value)
+    setOption(e.target.value)
+  }
+  return (
+    <>
+      <form>
+        <section>
+          <label htmlFor="ingen">Ingen</label>
+          <input
+            type="radio"
+            id="ingen"
+            name="kategori"
+            value="ingen"
+            onClick={handleOptionChange}
+          />
+          <label htmlFor="alder">Alder</label>
+          <input
+            type="radio"
+            id="alder"
+            name="kategori"
+            value="alder"
+            onClick={handleOptionChange}
+          />
+          <label htmlFor="kjonn">Kjønn</label>
+          <input
+            type="radio"
+            id="kjonn"
+            name="kategori"
+            value="kjonn"
+            onClick={handleOptionChange}
+          />
+          <label htmlFor="klasse">Klasse</label>
+          <input
+            type="radio"
+            id="klasse"
+            name="kategori"
+            value="klasse"
+            onClick={handleOptionChange}
+          />
+        </section>
+      </form>
+    </>
+  )
+}
