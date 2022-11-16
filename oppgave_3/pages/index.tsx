f3c37c8685aae6382ac7f486794f8231a39e433d
nattis <77730450+nattis@users.noreply.github.com>
2022-11-16 18:25:47 +0100

    laget komponenter

--- oppgave_3/pages/index.tsx
+++ oppgave_3/pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from 'next'
+import Weeks from '../components/Weeks'
 
 const Home: NextPage = () => {
   return (
     <main>
       <h1>Lunsjkalender</h1>
+      <Weeks />
     </main>
   )
 }
