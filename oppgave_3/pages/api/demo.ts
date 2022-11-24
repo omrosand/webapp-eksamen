810fc7fc5399a57e455ae268026d82f071f3587c
Ole M. Røsand <77734811+omrosand@users.noreply.github.com>
2022-11-24 14:45:16 +0100

    seeding

--- oppgave_3/pages/api/demo.ts
+++ oppgave_3/pages/api/demo.ts
@@ -1,42 +1,89 @@
 import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
 import { employees } from '../../data/employees'
 import lunch from '../../data/lunch.json'
 
-const prisma = new PrismaClient()
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  switch (req.method?.toLowerCase()) {
+    case 'post':
+      const prisma = new PrismaClient()
 
-export async function main() {
-  console.log(`Start seeding ...`)
+      async function main() {
+        console.log(`Start seeding ...`)
+        await prisma.day.deleteMany({})
+        await prisma.week.deleteMany({})
+        await prisma.year.deleteMany({})
+        await prisma.employee.deleteMany({})
 
-  for (const employee of employees) {
-    await prisma.employee.create({
-      data: { name: employee.name, rules: employee.rules },
-    })
-  }
-    const year = await prisma.year.create({
-      data: {}
-    })
-    
-  Object.entries(lunch.year).map(async ([weekKey, week]) => {
-      const weekPrisma = await prisma.week.create({
-       data: {week: Number(weekKey), yearId: year.id}
+        const year = await prisma.year.create({
+          data: {},
+        })
+
+        for (const employee of employees) {
+          await prisma.employee.create({
+            data: {
+              id: employee.id.toString(),
+              name: employee.name,
+              rules: employee.rules,
+            },
+          })
+        }
+
+        for (const [weekKey, week] of Object.entries(lunch.year)) {
+          const weekPrisma = await prisma.week.create({
+            data: {
+              week: Number(weekKey),
+              year: {
+                connect: {
+                  id: year.id,
+                },
+              },
+            },
+          })
+
+          for (const [dayKey, employee] of Object.entries(week.week)) {
+            if (employee) {
+              await prisma.day.create({
+                data: {
+                  name: dayKey,
+                  week: {
+                    connect: {
+                      id: weekPrisma.id,
+                    },
+                  },
+                  employee: {
+                    connect: {
+                      id: employee?.id.toString(),
+                    },
+                  },
+                },
+              })
+            }
+          }
+        }
+        console.log(`Seeding finished.`)
+      }
+
+      main()
+        .catch((e) => {
+          console.error(e)
+          process.exit(1)
+        })
+        .finally(async () => {
+          await prisma.$disconnect()
+        })
+
+      return res.status(200).json({
+        success: true,
+        resource: '/demo',
+        data: { message: 'Seeding was successful' },
       })
-
-    Object.entries(week.week).map(async ([dayKey, employee]) => {
-      if(employee) {
-        await prisma.day.create({
-        data: {name: dayKey, weekId: weekPrisma.id, employeeId: employee.id.toString()}
-      })}
-    })
-  })
-
-  console.log(`Seeding finished.`)
+    default:
+      return res
+        .status(400)
+        .json({ success: false, error: 'Only POST method allowed' })
+  }
 }
-
-main()
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
